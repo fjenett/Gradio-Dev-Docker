a282f296aa766119c852afb096c61713246193fd
Florian Jenett <florian.jenett@hs-mainz.de>
2023-01-06 19:56:34 +0100

    fix package.json

--- run_dev.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "Starting frontend dev server"
-cd ui && \
-  pnpm --filter @gradio/app dev --host 0.0.0.0 0<&- &>/dev/null &
-
-echo "Starting backend dev server"
-uvicorn app:app --port 7860 --reload --log-level warning --reload-dir "/gradio-dev/gradio/gradio" --reload-dir "."
---
+++ start-dev-server.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+mkdir ./logs
+
+echo "Starting frontend dev server"
+cd ui && \
+  pnpm --filter @gradio/app dev --host 0.0.0.0 0<&- &>logs/logs-frontend.log &
+
+echo "Starting backend dev server"
+uvicorn app:app --port 7860 --reload --log-level warning --reload-dir "/gradio-dev/gradio/gradio" --reload-dir "." 0<&- &>logs/logs-backend.log &
+
+tail -f /dev/null
